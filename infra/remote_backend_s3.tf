d78f40a4e1ad1ddd678a1fcc2c4d3d23ccf8012e
yourfavengineer <kendall8584@yahoo.com>
2024-01-24 22:03:35 -0800

    change bucket region in remote_backend_s3

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-jenkinsapi-project-s3"
     key    = "devops-jenkins-restapi/terraform.tfstate"
-    region = "eu-central-1"
+    region = "eu-west-1"
   }
 }
